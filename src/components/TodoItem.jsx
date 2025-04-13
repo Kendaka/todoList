68ffbf2c46f5a93ed6287c542e350bd03e70dfaa
Kendaka <barengkendrick4@gmail.com>
2025-04-13 21:31:44 +0400

    useState for linethrough

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,6 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 
 const todoItems = (props) => {
+
+    const changeTextStyle = () => {
+
+    }
+
     return (
         <div>
             <li>{props.text}</li>
